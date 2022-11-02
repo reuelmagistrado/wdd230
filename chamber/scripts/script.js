010ccf6eb95064b0dd8f66bdac7e6f17f57281d7
reuelmagistrado <mag21010@byui.edu>
2022-11-02 18:42:14 +0800

    fixed lazy loading

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -42,3 +42,54 @@ if (currentDay === 1 || currentDay === 2) {
   banner.textContent =
     "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.src = image.getAttribute("data-src");
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+// initialize display element
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits;
+} else {
+  visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
